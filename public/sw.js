9ea0ab09d5a4a4e84c7193ecb5df294281edf476
rdkarlos <rdkarlos@hotmail.com>
2026-06-14 14:19:45 -0500

    fix cache service worker v2

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,20 @@
-const CACHE_NAME = 'camino-de-fe-v1';
+const CACHE_NAME = 'camino-de-fe-v2';
 const urlsToCache = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
