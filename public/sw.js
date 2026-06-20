95554c2358e04a333b1f02e65fc63957fcffacfc
rdkarlos <rdkarlos@hotmail.com>
2026-06-20 14:41:59 -0500

    mostrar lecturas en inicio

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'camino-de-fe-v3';
+const CACHE_NAME = 'camino-de-fe-v4';
 const urlsToCache = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', event => {
