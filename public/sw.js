9c774d03c4ca7c17c562154b4291c1985e39e663
rdkarlos <rdkarlos@hotmail.com>
2026-06-29 08:55:08 -0500

    fix: service worker v10

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'camino-de-fe-v9';
+const CACHE_NAME = 'camino-de-fe-v10';
 const urlsToCache = ['/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', event => {
