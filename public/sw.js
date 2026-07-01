3cff8c3b0962bfddb4866fa1b227fe2532891861
rdkarlos <rdkarlos@hotmail.com>
2026-07-01 06:56:12 -0500

    Arreglar fecha del corderito y otras cosas

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'camino-de-fe-v20';
+const CACHE_NAME = 'camino-de-fe-v21';
 const urlsToCache = ['/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', event => {
