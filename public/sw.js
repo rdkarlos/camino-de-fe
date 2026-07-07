13b9c688f4069d2e32558340703d47acd9e21a8c
rdkarlos <rdkarlos@hotmail.com>
2026-07-06 20:53:49 -0500

    Implementacion del brand book 1

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'camino-de-fe-v16';
+const CACHE_NAME = 'camino-de-fe-v17';
 const urlsToCache = ['/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', event => {
